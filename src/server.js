73fe577ff643bee3786ea518b62fd0632ceb1a1c
devhak <devhak.enzo@gmail.com>
2024-01-27 20:47:27 +0900

    - public directory ignoring - server.js add app setting

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 import express from "express";
+import path from 'path';
+const __dirname = path.resolve();
 
 const app = express();
 
-console.log("hello");
-app.listen(3000);
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+app.listen(3000, handleListen);
