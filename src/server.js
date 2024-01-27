f75c73bc8cdef50ed292ef1fb2d24a21c9cbdccd
devhak <devhak.enzo@gmail.com>
2024-01-27 21:01:36 +0900

    directory 경로 수정

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const __dirname = path.resolve();
 const app = express();
 
 app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
+app.set("views", __dirname + "/src/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 
